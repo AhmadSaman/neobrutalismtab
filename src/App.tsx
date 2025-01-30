547c197893d468bd4c17ae87dfc438c058e9a872
Ahmad Saman <55833403+AhmadSaman@users.noreply.github.com>
2025-01-30 22:21:37 +0300

    feat: random theme when clicking clock

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,28 @@ import { useEffect, useState } from "react";
 import { useThemeStore } from "@/theme";
 import { cn } from "@/lib/utils";
 import NumberFlow, { NumberFlowGroup } from "@number-flow/react";
-import { Card, CardContent } from "./components/ui/card";
+import { Button } from "./components/ui/button";
+
+function getRandomTheme() {
+  const colors = [
+    "pastel-orange",
+    "light-gold",
+    "",
+    "light-gold",
+    "bittersweet",
+    "kiwi-green",
+    "purple-mimosa",
+  ];
+
+  const randomIndex = Math.floor(Math.random() * colors.length);
+  return colors[randomIndex];
+}
 
 function App() {
   const {
-    theme: { hasDots },
+    theme: { hasDots, type },
+    toggleDots,
+    setType,
   } = useThemeStore((state) => state);
 
   const [time, setTime] = useState({
@@ -26,10 +43,16 @@ function App() {
     return () => clearInterval(timer);
   }, []);
 
+  const handleClick = () => {
+    setType(getRandomTheme());
+    toggleDots();
+  };
+
   return (
     <div
       className={cn(
-        "h-screen w-screen flex flex-col bittersweet bg-bg font-poppins",
+        "h-screen w-screen flex flex-col bg-bg font-poppins",
+        type,
         {
           "bg-[radial-gradient(#80808080_1px,transparent_1px)] [background-size:16px_16px]":
             hasDots,
@@ -37,21 +60,19 @@ function App() {
       )}
     >
       <div className="flex flex-col m-auto gap-3">
-        <Card>
-          <CardContent className="w-[300px] content-center font-black text-7xl text-center p-2 m-auto">
-            <NumberFlowGroup>
-              <NumberFlow
-                value={time.hh}
-                format={{ minimumIntegerDigits: 2 }}
-              />
-              <NumberFlow
-                prefix=":"
-                value={time.mm}
-                format={{ minimumIntegerDigits: 2 }}
-              />
-            </NumberFlowGroup>
-          </CardContent>
-        </Card>
+        <Button
+          onClick={handleClick}
+          className="w-[300px] h-fit content-center font-black text-7xl text-center p-2 m-auto"
+        >
+          <NumberFlowGroup>
+            <NumberFlow value={time.hh} format={{ minimumIntegerDigits: 2 }} />
+            <NumberFlow
+              prefix=":"
+              value={time.mm}
+              format={{ minimumIntegerDigits: 2 }}
+            />
+          </NumberFlowGroup>
+        </Button>
         <div className="text-center flex flex-col gap-10">
           <p className="text-xl font-semibold dark:text-main">
             {new Date().toLocaleDateString(undefined, {
@@ -61,6 +82,25 @@ function App() {
               day: "numeric",
             })}
           </p>
+          <div className="grid grid-cols-5 gap-4">
+            {/* {shortcuts.map((url) => (
+              <Button asChild variant="reverse" size={"icon"} className="p-1">
+                <a
+                  key={url}
+                  href={url}
+                  rel="noopener noreferrer"
+                  className="size-full"
+                >
+                  <img
+                    // TODO: find better way to get favicon
+                    src={`${new URL(url).origin}/favicon.ico`}
+                    alt="favicon"
+                    className="rounded overflow-hidden"
+                  />
+                </a>
+              </Button>
+            ))} */}
+          </div>
         </div>
       </div>
     </div>
--- src/theme.ts
+++ src/theme.ts
@@ -3,19 +3,27 @@ import { persist } from "zustand/middleware";
 
 export type ThemeType = {
   hasDots: boolean;
+  type: string;
 };
 
 export type ThemeStateType = {
   theme: ThemeType;
   toggleDots: () => void;
+  setType: (newThemeType: string) => void;
 };
 
 export const useThemeStore = create<ThemeStateType>()(
   persist(
     (set) => ({
-      theme: { hasDots: true },
+      theme: { hasDots: true, type: "" },
       toggleDots: () =>
-        set((state) => ({ theme: { hasDots: !state.theme.hasDots } })),
+        set((state) => ({
+          theme: { ...state.theme, hasDots: !state.theme.hasDots },
+        })),
+      setType: (newThemeType: string) =>
+        set((state) => ({
+          theme: { ...state.theme, type: newThemeType },
+        })),
     }),
     { name: "theme-config" }
   )
